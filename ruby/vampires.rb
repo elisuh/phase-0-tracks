fa12915d136e8c8c3ee04f90cf7f26a02d54b6d9
elisuh <elisaverna@gmail.com>
2016-05-28 15:02:49 -0400

    add allerge check loops

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,22 +14,30 @@ until i == employee_number
   puts "What year were you born?"
   birth_year = gets.chomp.to_i
 
-  def age_matches_year
-    if 2016 - birth_year == age
-      age_matches_year = true
-    else
-      agematches_year = false
-  end
-
-
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
   garlic_bread = gets.chomp
 
   puts "Would you like to enroll in the company’s health insurance? (y/n)"
   insurance = gets.chomp
 
-  puts "Please list any allergies, one at a time."
-  allergies = gets.chomp
+  allergies = " "
+  puts "Please list any allergies in lowercase letters, one at a time. Type 'done' when finished."
+  while allergies != "sunshine" && allergies != "done"
+    allergies = gets.chomp
+  end
+  if allergies == "done"
+    puts "Thank you! Processing."
+  end
+  if allergies == "sunshine"
+    puts "Probably a vampire."
+  end
+
+  if 2016 - birth_year == age
+    age_matches_year = true
+  else
+    age_matches_year = false
+  end
+
 
   if age_matches_year = true && garlic_bread == "y" || insurance == "y"
     puts "Probably not a vampire."
