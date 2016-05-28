95b3d5403c40f4b4fa987452d819565c222fc3fe
elisuh <elisaverna@gmail.com>
2016-05-28 12:05:04 -0400

    add conditions to vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,22 @@
 puts "What is your name?"
+name = gets.chomp
 
 puts "What year were you born?"
+birth_year = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
+insurance = gets.chomp
+
+if garlic_bread == "y" || birth_year > 1910 || insurance == "y"
+  puts "Probably not a vampire."
+if garlic_bread == "n" || birth_year < 1910 || insurance == "n"
+  puts "Probably a vampire."
+if garlic_bread == "n" && birth_year < 1910 && insurance == "n"
+  puts "Almost certainly a vampire!"
+else
+  puts "Results inconclusive."
+end
+
