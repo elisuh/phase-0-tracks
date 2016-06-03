2ce33424e7ee2551dfc9ed26495ce83fd0aff885
elisuh <elisaverna@gmail.com>
2016-06-02 21:42:42 -0400

    pseudocode hashes

---
+++ ruby/hashes.rb
@@ -0,0 +1,48 @@
+# PSEUDOCODE FOR INTERIOR DESIGN PROGRAM
+
+#
+# 	Ask client for name
+#   Assign name to :name 
+
+# 	Ask client for age, convert to integer 
+# 	Assign age to age
+
+#	Ask client for number of children, convert to integer
+# 	Assign child number to child_number
+
+# 	Ask client for decor theme
+# 	Assign to decor_theme
+
+# 	Ask client if they like pastels (y/n)
+# 	Assign input to liked_pastels
+# 	Define "y" as true and "n" as false
+
+# 	Print hash design_client with variables assigned to to corresponding symbols
+
+# 	Ask user if there are any keys they need to update
+# 	If "none" 
+	#		continue without doing anything
+	# 	elsif "name"
+	# 		ask client for their name
+	# 		assign new name to name variable
+	# 	elsif "decor_theme" 
+	#		ask client for decor theme
+	# 		assign new theme to decor_theme variable
+	# 	elsif "age"
+	#		ask client for new age
+	#		convert string to integer and assign new age to age variable
+	# 	elsif "child_number"
+	# 		ask client how many children they have
+	# 		convert string to integer and assign new number to child_number variable
+	# 	elsif "liked_pastels"
+	# 		ask client if they like pastels (y/n)
+	# 		define "y" as true and "n" as false
+	# 	else print "I didn't understand you"
+# end loop
+# print new hash
+# end program
+#
+#
+#
+#
+
