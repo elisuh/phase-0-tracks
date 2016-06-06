7b23ba0b0317d1f1d336348d67db6375d0262900
elisuh <elisaverna@gmail.com>
2016-06-05 22:22:31 -0400

    add bullshit code that works but doesn't meet requirements

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,16 +1,20 @@
-#define variables for the user's name
-#swap user's first and last name
-#change the vowels to the next vowel and the consonants to the next consonants
-#return the alias
+# define variables for the user's name
+# swap user's first and last name
+# split name into letters
+# change the vowels to the next vowel 
+# change the consonants to the next consonants
+# combine letters
+# return the alias
 
 ##############
 
-#define variable for the user's name and split it into first and last name
-name = "Elisa Verna".split(' ')
-#swap user's first and last name
-name[0], name[1] = name[1], name[0]
-#split name into letters
-new_name = name[0].chars + name[1].chars
+# define variable for the user's name and split it into first and last name
 
+puts "What is your first name?"
+first_name = gets.chomp.split('')
+puts "What is your last_name name?"
+last_name = gets.chomp.split('')
 
-
+# swap user's first and last name and shift each letter one character
+new_name = last_name.map!{|letter| letter.next}.join('') + " " + first_name.map!{|letter| letter.next}.join('')
+puts "Your alias is #{new_name}"
