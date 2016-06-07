db85006875afd45cffb554cfd5d74020921705b1
elisuh <elisaverna@gmail.com>
2016-06-07 19:01:04 -0400

    psuedocode puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Color: varies
+# Housebroken: no
+# Name: varies
+# Legs: 4
+# Energe level: high
+
+# BEHAVIOR -----------------------
+# Tail-wag
+# Slobbers
+# Curious
+# Annoying
+
+class Puppy
+end
