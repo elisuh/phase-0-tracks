390e6be308c0ee363e22ce0bdbf27556632058b9
elisuh <elisaverna@gmail.com>
2016-06-11 19:10:48 -0400

    psuedocode wrestler class

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+## WRESTLEFRIENDS ##
+# Define class Westler
+	#Initialize a new wrestler
+		#Make attribute @name the variable name
+		#Make attribute @weight the variable weight
+		#Make attribute @age the variable age
+		#Make attribute @hometown the variable hometown
+	#Make a personality method that takes a variable heel_face
+		#If heel, say or do something mean
+		#If face, say or do something nice
+	#Make a finishing move method that takes a variable finishing_move
+	#Make a body method that takes the parameters muscles (an integer) and attractiveness (a string)
+
+
