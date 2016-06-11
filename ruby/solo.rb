535b753d340de67d73132941628b6937af3ff077
elisuh <elisaverna@gmail.com>
2016-06-11 19:33:37 -0400

    create wrestler class with methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -5,17 +5,51 @@
 		#Make attribute @weight the variable weight
 		#Make attribute @age the variable age
 		#Make attribute @hometown the variable hometown
-	#Make a personality method that takes a variable heel_face
+	#Make a personality method that takes a variable heel_face and charisma (integer)
 		#If heel, say or do something mean
 		#If face, say or do something nice
 	#Make a body method that takes the parameters muscles (an integer) and attractiveness (a string)
-	#Make a finishing move method that takes a variable finishing_move
-	
+	#Make a wrestling moves method that takes a variable finishing_move (string) and technical ability (integer)
+
 #Ask user how many wrestlers they would like to make
 #Ask for variables for parameters in initialization
 #Initialize that number of wrestlers
 #Store new instances in an array
 #Ask users for parameters for personality, finishing move and body methods
+
+class Wrestler
+	def initialize(name, weight, age, hometown, gender)
+		@name = name
+		@weight = weight.to_i
+		@age = age.to_i
+		@hometown = hometown
+		@gender = gender
+	end
+	def personality(heel_face, charisma)
+			if heel_face == "heel"
+				puts "#{@name} just cheated to win the championship!"
+			elsif heel_face == "face"
+				puts "#{@name} gave all of the children in the front row high fives."
+			else puts "I didn't understand you, please type 'heel' or 'face'."
+			end
+		@heel_face = heel_face
+		@charisma = charisma.to_i
+	end
+	def body(muscles, attractiveness)
+		@muscles = muscles.to_i
+			if @muscles > 5
+				puts "Wow, #{@name} is a meaty #{@gender}!"
+			end
+		@attractiveness = attractiveness
+	end
+	def wrestling_moves(finishing_move, technical)
+		@finishing_move = finishing_move
+		@technical = technical.to_i
+			if @tehcnical < 5
+				puts "#{@name} needs to spend more time training."
+			end
+	end
+end
 
 
 
