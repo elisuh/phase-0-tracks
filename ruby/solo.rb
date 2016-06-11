5516b8b4620991d78c23dcc913cb33c0724df152
elisuh <elisaverna@gmail.com>
2016-06-11 19:13:58 -0400

    update psuedocode

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -8,7 +8,14 @@
 	#Make a personality method that takes a variable heel_face
 		#If heel, say or do something mean
 		#If face, say or do something nice
-	#Make a finishing move method that takes a variable finishing_move
 	#Make a body method that takes the parameters muscles (an integer) and attractiveness (a string)
+	#Make a finishing move method that takes a variable finishing_move
+	
+#Ask user how many wrestlers they would like to make
+#Ask for variables for parameters in initialization
+#Initialize that number of wrestlers
+#Store new instances in an array
+#Ask users for parameters for personality, finishing move and body methods
+
 
 
