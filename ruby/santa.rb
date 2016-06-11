0dc9cc20aded0ce59e830ce4d7a9d92a6e61cb39
elisuh <elisaverna@gmail.com>
2016-06-11 14:40:47 -0400

    add diverse initialization code for santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,8 +4,8 @@ class Santa
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 	
 	def speak
@@ -16,4 +16,11 @@ class Santa
 		puts "That was a good #{cookie}!" 
 	end
 	
+end
+
+santas = []
+santa_genders = ["male", "female", "seahorse", "intergender", "agender"]
+santa_ethnicities =["Chinese", "Latino", "white", "Korean-American", "African-Canadian"]
+example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
