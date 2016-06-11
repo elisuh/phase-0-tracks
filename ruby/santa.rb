7b76210bc870d159c2f8c021cfc52ab8059b56d8
elisuh <elisaverna@gmail.com>
2016-06-11 16:09:05 -0400

    create program that generates 500 random santas and stores in an array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,16 @@
+# DBC 6.3
+
 class Santa
 
-	attr_reader :age, :ethnicity
-	attr_accessor :gender
+	attr_reader :ethnicity
+	attr_accessor :gender, :age
 	
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = rand(140)
 	end
 	
 	def speak
@@ -18,19 +20,19 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!" 
 	end
-
+# setter method
 	def celebrate_birthday
 		@age += 1
 	end
-
+# setter method
 	def get_mad_at(name)
-		@reindeer_ranking = @reindeer_ranking.delete_if {|reindeer| reindeer == name}
+		@reindeer_ranking = @reindeer_ranking.delete_if {|i| i == name}
 		@reindeer_ranking = @reindeer_ranking.insert(-1, name)
 	end
 	
 end
 
-##RELEASE 1 DIVER CODE
+## RELEASE 1 DIVER CODE
 
 # santas = []
 # santa_genders = ["male", "female", "seahorse", "intergender", "agender"]
@@ -39,8 +41,17 @@ end
 # 	santas << Santa.new(example_genders[i], example_ethnicities[i])
 # end
 
-##RELEASE 2 DRIVER CODE
-santa = Santa.new("male", "Latino")
-p santa.get_mad_at("Rudolph")
-p santa.gender = "female"
-p santa.celebrate_birthday
+## RELEASE 2 DRIVER CODE
+# santa = Santa.new("male", "Latino")
+# p santa.get_mad_at("Rudolph")
+# p santa.gender = "female"
+# p santa.celebrate_birthday
+
+## RELEASE 3 DRIVER CODE
+santas = []
+santa_genders = ["male", "female", "seahorse", "intergender", "agender"]
+santa_ethnicities =["Chinese", "Latino", "white", "Korean-American", "African-Canadian"]
+500.times do 
+	santas << Santa.new(santa_genders.sample, santa_ethnicities.sample)
+end 
+p santas
