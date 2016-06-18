a55a3312bb969a74b7486e296abdb7215978b0ee
elisuh <elisaverna@gmail.com>
2016-06-18 16:19:28 -0400

    initial commit, psuedocode release 0

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+// DBC 7.3
+
+// Release 0 - Function that returns the longest string in an array
+	// Define a function that takes one parameter (an array)
+	// Determine the length of the array
+	// For each index in the array, find the length of the index. 
+	// Determine which index length is the greatest number
+	// Return the value of that index
+
+
+function longestString(array) {
+	for (var i = 0; i < array.length; i++) {
+		// find length of each index
+	}
+	// return index of longest string
+}
