bdebcb17b917e949b7017129856f008f15161e51
elisuh <elisaverna@gmail.com>
2016-06-24 21:43:13 -0400

    add feed_pet method to update if pet has been fed

--- databases/feeding_schedule/feeding_schedule.rb
+++ databases/feeding_schedule/feeding_schedule.rb
@@ -24,11 +24,29 @@ SQL
 # create a table 
 db.execute(create_table)
 
-# Create user interface
-	#Add pet info
+#Add pet info
 def add_pet(db, name, animal, feeding_time, fed)
-  db.execute("INSERT INTO pets (name, animal, feeding_time, fed) VALUES (?, ?, ?, ?)", [name, animal, feeding_time, fed])
+	db.execute("INSERT INTO pets (name, animal, feeding_time, fed) VALUES (?, ?, ?, ?)", [name, animal, feeding_time, fed])
 end
 
-	#Allow user to update and retrieve if pet has been fed
-	#Allow user to exit when done
+#Allow user to update and retrieve if pet has been fed
+def feed_pet(db, pet_name, fed)
+	db.execute("UPDATE pets SET (fed) WHERE name=pet_name")
+end
+
+#Create user interface
+
+puts "Let's add a pet to your feeding schedule! Type 'done' when finished. What is the name of your pet?"
+name = gets.chomp
+puts "What type of animal are they?"
+animal = gets.chomp
+puts "What time(s) should they be fed?"
+feeding_time = gets.chomp
+fed = false
+
+# 2.times do 
+# 	add_pet(db, name, animal, feeding_time, fed)
+# end
+
+
+#Allow user to exit when done
